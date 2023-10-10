792db019fea689e7c2bfde26076356a901ead647
julio189 <julio.c.jesus@hotmail.com>
2023-10-10 13:28:28 -0300

    feat: add user entitie

---
+++ src/entities/User.ts
@@ -0,0 +1,18 @@
+import { uuid } from 'uuidv4'
+
+export class User {
+  public readonly id: string | undefined
+  public name: string
+  public email: string
+  public password: string
+
+  constructor({ name, email, password, id }: User) {
+    this.name = name
+    this.email = email
+    this.password = password
+
+    if (!id) {
+      this.id = uuid()
+    }
+  }
+}
