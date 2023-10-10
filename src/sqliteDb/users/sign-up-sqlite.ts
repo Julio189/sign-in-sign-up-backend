fd095cf113eefc2b331a8cb7771c70f45cad48bf
julio189 <julio.c.jesus@hotmail.com>
2023-10-10 15:14:30 -0300

    feat: add sign up sqlite

---
+++ src/sqliteDb/users/sign-up-sqlite.ts
@@ -0,0 +1,20 @@
+import { prismaClient } from '../../database/client'
+import { User } from '../../entities/User'
+import { ISignUpRepository } from '../../modules/users/signUp/repositories/sign-up-repository'
+
+export class SignUpSqlite implements ISignUpRepository {
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await prismaClient.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    return user
+  }
+
+  async save(user: User): Promise<void> {
+    await prismaClient.user.create({
+      data: user,
+    })
+  }
+}
