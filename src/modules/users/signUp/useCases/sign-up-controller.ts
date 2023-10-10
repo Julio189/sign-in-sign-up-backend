cc4a17f78a8f66ecffd80922fae50e96ca7a9546
julio189 <julio.c.jesus@hotmail.com>
2023-10-10 15:13:24 -0300

    feat: add sign up controller

---
+++ src/modules/users/signUp/useCases/sign-up-controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+import { SignUpService } from './sign-up-service'
+
+export class SignUpController {
+  private signUpService: SignUpService
+
+  constructor(signUpService: SignUpService) {
+    this.signUpService = signUpService
+  }
+
+  async handle(request: Request, response: Response) {
+    const { name, email, password } = request.body
+
+    try {
+      await this.signUpService.execute({
+        name,
+        email,
+        password,
+      })
+      return response.status(201).send('User Created!')
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      return response.status(error.statusCode).json({ error })
+    }
+  }
+}
