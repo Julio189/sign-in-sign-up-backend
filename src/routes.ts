c99000b009ff935b7ec23ac5b0f6e5ba4ffc0bd9
julio189 <julio.c.jesus@hotmail.com>
2023-10-11 10:02:46 -0300

    feat: add get users route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import { signUpController } from './modules/users/signUp/useCases'
+import { getUsersController } from './modules/users/getAll/useCases'
 
 const router = Router()
 
 router.post('/signUp', (req, res) => {
   return signUpController.handle(req, res)
+})
+
+router.get('/users', (req, res) => {
+  return getUsersController.handle(req, res)
 })
 
 export { router }
