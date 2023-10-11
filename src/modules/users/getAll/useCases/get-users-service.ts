f5e4b8b045ca91f4f069f3c5dac0f70eee2d06f6
julio189 <julio.c.jesus@hotmail.com>
2023-10-11 10:02:02 -0300

    feat: add get users service

---
+++ src/modules/users/getAll/useCases/get-users-service.ts
@@ -0,0 +1,19 @@
+import { AppError } from '../../../../shared/errors/app-errors'
+import { IGetUsersRepository } from '../repositories/get-users-repository'
+
+export class GetUsersService {
+  private getUsersRepository: IGetUsersRepository
+
+  constructor(getUsersRepository: IGetUsersRepository) {
+    this.getUsersRepository = getUsersRepository
+  }
+
+  async execute() {
+    const getUsers = await this.getUsersRepository.getAll()
+
+    if (!getUsers) {
+      throw new AppError('There is no users', 400)
+    }
+    return getUsers
+  }
+}
