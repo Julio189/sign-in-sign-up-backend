58a2f5c90aaa7f3d8c0927938f9537c124908d28
julio189 <julio.c.jesus@hotmail.com>
2023-10-11 10:01:45 -0300

    feat: add get users sqlite

---
+++ src/sqliteDb/users/get-all-sqlite.ts
@@ -0,0 +1,10 @@
+import { prismaClient } from '../../database/client'
+import { User } from '../../entities/User'
+import { IGetUsersRepository } from '../../modules/users/getAll/repositories/get-users-repository'
+
+export class GetUsersSqlite implements IGetUsersRepository {
+  async getAll(): Promise<User[]> {
+    const getUsers = await prismaClient.user.findMany()
+    return getUsers
+  }
+}
